8db35eead979d0d3e1db10d48ddd9a4735679140
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2020-02-11 16:54:53 -0300

    First commit

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI:
+    "mongodb://mongoadmin:secret@localhost:27017/blog_everyone?authSource=admin",
+  mongoUser: "mongoadmin",
+  mongoSecret: "secret",
+  cookieKey: "123123123"
+};
---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  mongoUser: process.env.MONGO_USER,
+  mongoSecret: process.env.MONGO_SECRET,
+  cookieKey: process.env.COOKIE_KEY
+};
