2d61483e410d8e0a27af9f28a0a68ac9f3fe563b
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2020-02-12 23:33:16 -0300

    Add application testing support

---
+++ tests/helpers/page.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+
+const sessionFactory = require('../factories/sessionFactory');
+const userFactory = require('../factories/userFactory');
+
+class Page {
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+
+    const page = await browser.newPage();
+    const customPage = new Page(page);
+
+    return new Proxy(customPage, {
+      get: function(target, property) {
+        return customPage[property] || browser[property] || page[property];
+      }
+    });
+  }
+
+  constructor(page) {
+    this.page = page;
+  }
+
+  async login() {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+
+    await this.page.setCookie({ name: 'session', value: session });
+    await this.page.setCookie({ name: 'session.sig', value: sig });
+    await this.page.goto('localhost:3000');
+    const logoutBtn = `a[href='/auth/logout']`;
+    await this.page.waitFor(logoutBtn);
+  }
+
+  async getContentsOf(selector) {
+    if (!selector) return null;
+
+    return this.page.$eval(selector, el => el.innerHTML);
+  }
+}
+
+module.exports = Page;
